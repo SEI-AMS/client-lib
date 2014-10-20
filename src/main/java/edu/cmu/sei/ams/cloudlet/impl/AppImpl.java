7a33827c85a3685a8dd08556a8d56db7820aeb93
James Root <jamesroot@gmail.com>
2014-10-20 13:07:51 -0400

    Added some additional logging to AppImpl

--- src/main/java/edu/cmu/sei/ams/cloudlet/impl/AppImpl.java
+++ src/main/java/edu/cmu/sei/ams/cloudlet/impl/AppImpl.java
@@ -4,11 +4,12 @@ import edu.cmu.sei.ams.cloudlet.App;
 import edu.cmu.sei.ams.cloudlet.CloudletException;
 import edu.cmu.sei.ams.cloudlet.impl.cmds.GetAppCommand;
 import org.json.JSONObject;
+import org.slf4j.ext.XLogger;
+import org.slf4j.ext.XLoggerFactory;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+
 import static edu.cmu.sei.ams.cloudlet.impl.CloudletUtilities.*;
 
 /**
@@ -18,6 +19,7 @@ import static edu.cmu.sei.ams.cloudlet.impl.CloudletUtilities.*;
  */
 public class AppImpl implements App
 {
+    private static final XLogger log = XLoggerFactory.getXLogger(AppImpl.class);
     private final CloudletCommandExecutor mExecutor;
     private final JSONObject json;
 
@@ -45,6 +47,8 @@ public class AppImpl implements App
         this.serviceId = getSafeString("service_id", json);
         this.appVersion = getSafeString("app_version", json);
         this.description = getSafeString("description", json);
+
+        log.debug("Created app: " + this);
     }
 
     /**
@@ -141,8 +145,9 @@ public class AppImpl implements App
      * {@inheritDoc}
      */
     @Override
-    public File downloadApp(File outputDirectory)  throws CloudletException
+    public File downloadApp(File outputDirectory) throws CloudletException
     {
+        log.entry(outputDirectory);
         //Verify the directory before we use it, default to java.io.tmpdir
         File _outDirectory = outputDirectory;
         if (outputDirectory == null || !outputDirectory.exists())
@@ -150,26 +155,41 @@ public class AppImpl implements App
 
         File outFile = new File(_outDirectory, this.getName() + ".apk");
 
-        //If the file already exists, not really a reason to download it again
+        File ret = null;
+
+        // If the file already exists, we will return it
         if (outFile.exists())
-            return outFile;
+        {
+            ret = outFile;
+        } else
+        {
+            GetAppCommand cmd = new GetAppCommand(this.getId(), outFile);
+            //Response will contain the MD5 sum for validation
+            String md5 = mExecutor.executeCommand(cmd);
 
-        GetAppCommand cmd = new GetAppCommand(this.getId(), outFile);
-        //Response will contain the MD5 sum for validation
-        String md5 = mExecutor.executeCommand(cmd);
+            //If we dont have an MD5 sum, let it through
+            if (this.getMD5Sum() == null || this.getMD5Sum().trim().length() == 0)
+                ret = outFile;
+                //If the md5 sum matches, let it through
+            else if (md5 != null && md5.equalsIgnoreCase(this.getMD5Sum()))
+                ret = outFile;
+            else
+            {
+                CloudletException e = new CloudletException(
+                        String.format("MD5 mismatch. Expecting '%s' but got '%s'", this.getMD5Sum(), md5)
+                );
+                log.exit(e);
+                throw e;
+            }
+        }
+        log.exit(ret);
+        return ret;
 
-        //If we dont have an MD5 sum, just let it through
-        if (this.getMD5Sum() == null || this.getMD5Sum().trim().length() == 0)
-            return outFile;
-        //If the md5 sum matches, let it through
-        if (md5 != null && md5.equalsIgnoreCase(this.getMD5Sum()))
-            return outFile;
-
-        throw new CloudletException(String.format("MD5 mismatch. Expecting '%s' but got '%s'", this.getMD5Sum(), md5));
     }
 
     /**
      * {@inheritDoc}
+     *
      * @return
      */
     @Override
